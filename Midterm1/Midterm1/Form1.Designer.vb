a41b04a6c0b88ea21ea51f917aed7a8f0b5fe642
MallipeddiFall2015 <vijayprakashmallipeddi@Vijayprakashs-MacBook-Pro.local>
2015-11-14 10:44:47 -0800

    MidTerm In class

---
+++ Midterm1/Midterm1/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿
+Private Sub sum_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+End Sub
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.number1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.number2 = New System.Windows.Forms.TextBox()
+        Me.Result = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "FirstNum"
+        '
+        'number1
+        '
+        Me.number1.Location = New System.Drawing.Point(198, 35)
+        Me.number1.Name = "number1"
+        Me.number1.Size = New System.Drawing.Size(100, 22)
+        Me.number1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "SecondNum"
+        '
+        'number2
+        '
+        Me.number2.Location = New System.Drawing.Point(198, 81)
+        Me.number2.Name = "number2"
+        Me.number2.Size = New System.Drawing.Size(100, 22)
+        Me.number2.TabIndex = 3
+        '
+        'Result
+        '
+        Me.Result.Location = New System.Drawing.Point(198, 221)
+        Me.Result.Name = "Result"
+        Me.Result.Size = New System.Drawing.Size(100, 22)
+        Me.Result.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(107, 226)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 17)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Result"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(76, 135)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(54, 21)
+        Me.RadioButton1.TabIndex = 10
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Add"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(148, 134)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(51, 21)
+        Me.RadioButton2.TabIndex = 11
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "min"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(225, 135)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(55, 21)
+        Me.RadioButton3.TabIndex = 12
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "mult"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(306, 134)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(58, 21)
+        Me.RadioButton4.TabIndex = 13
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Subt"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(198, 171)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(608, 255)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Result)
+        Me.Controls.Add(Me.number2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.number1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents number1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents number2 As System.Windows.Forms.TextBox
+    Friend WithEvents Result As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim num1, num2 As Integer
+        num1 = Val(number1.Text)
+        num2 = Val(number2.Text)
+
+    End Sub
+
+
---
+++ Midterm1/Midterm1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles number1.TextChanged
+        Dim num1, num2 As Integer
+        num1 = Val(number1.text)
+        num2 = Val(number2.text)
+
+        sum.text = num1 + num2
+
+    End Sub
+
+    Private Sub num2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles number2.TextChanged
+
+    End Sub
+End Class
